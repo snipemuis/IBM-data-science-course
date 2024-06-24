babac4b93f25de6added362c7fb68ff70e3c1787
Bas Muijzer <bas@easee.online>
2024-06-24 17:40:22 +0200

    first commit

---
+++ data_visualization/module5/DV0101EN-Final-Assign-Part-2-Questions.py
@@ -0,0 +1,10 @@
+#2.1 
+# Provide title of the dash application title as
+
+# Automobile Sales Statistics Dashboard
+
+# Make the heading center aligned
+# set color as #503D36
+# font size as 24
+# Sample: style={‘textAlign’: ‘left’, ‘color’: ‘#000000’, ‘font-size’: 0}
+# save as title.png
